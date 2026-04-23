7cbf390764d8259d7af28b49824fdc547bc8da5f
Perdigao-wander <oliveirawander1998@gmail.com>
2026-04-23 14:39:15 +0000

    novo comit

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -30,7 +30,7 @@ const Projects = () => {
             image: "/assets/Xina_za_header.png",
             tags: ["Next.js", "Supabase", "Socket.IO", "TypeScript", "Tailwind CSS", "Real-time Audio"],
             github: "#",
-            demo: "#"
+            demo: "https://xina-za-site-kt2d.vercel.app/"
         }
     ];
 
